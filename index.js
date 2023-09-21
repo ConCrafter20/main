42018a81eb349ee4884bebda22d4670142d013ea
Raphael Wegmann <raphael.wegmann81@gmail.com>
2023-09-21 21:08:52 +0200

    redo the whole thing

--- index.js
+++
@@ -1,9 +0,0 @@
-function secret() {
-    alert("Secret found!")
-}
-
-function openInNewTab(uri) {
-    window.open(uri, "_blank")
-}
-
-document.getElementById("secret").addEventListener("click", secret)
